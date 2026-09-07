1d7c9de9254b5b558123b03a858e732e798b6ee7
Iyan Z Armanda <150656877+iyanarmanda@users.noreply.github.com>
2026-09-07 07:45:53 +0700

    fix: remove lang in frontmatter script

--- scripts/new-post.js
+++ scripts/new-post.js
@@ -1,7 +1,7 @@
 /* This is a script to create a new post markdown file with front-matter */
 
-import fs from "fs";
-import path from "path";
+import fs from "node:fs";
+import path from "node:path";
 
 function getDate() {
 	const today = new Date();
@@ -50,7 +50,6 @@ image: ''
 tags: []
 category: ''
 draft: false 
-lang: ''
 ---
 `;
 
